12182651fc20a5cb84fcef013a64e327de02d939
Ariana Cabana <acabana.0904@gmail.com>
2017-06-02 15:07:25 -0500

    poopup listo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,25 @@
 // llamando el alert a travez de una etiqueta <a>
-$( () => {
-  $('a').click(function(e){
-    alert("Hola");
+// $( () => {
+//   $('a').click(function(e){
+//     alert("Hola");
+//   });
+//
+//   $('.popup-close')
+// });
+
+$( () => { //function() {}
+  $(".popup-link").on('click',(e) => {
+    e.preventDefault();
+    $('.brand-popup').show();
+    //$('.brand-popup').removeClass('hide');
+    //$('.brand-popup').addClass('show');
+
   });
 
-  $('.popup-close')
+  $(".brand-popup .popup-close").on('click',(e) => {
+    e.preventDefault();
+    $('.brand-popup').hide();
+    //$('.brand-popup').removeClass('show');
+    //$('.brand-popup').addClass('hide');
+  })
 });
